27aa36a9f9fdeaeff8412e08200f60cb735b1308
Lorraine B <lbondesignweb@gmail.com>
2023-04-24 16:14:51 +0200

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Atelier_Disney_Directives';
+
+
+aliments: string[] = ['Fish', 'Steack', 'Oignon', 'Chips'];
+alimentsEssentiels: boolean = true;
+
+
+constructor() { }
+
+}
